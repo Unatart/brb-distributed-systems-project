842cdd7054ac5add9e3c6376aa9dd8e5f48402ca
nutochkina <nutochkina@ivi.ru>
2020-05-12 21:44:23 +0300

    finish started page

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -6,17 +6,21 @@ import "./Home.css"
 export class Home extends React.Component {
     public render() {
         return (
-            <div className="main">
-                <Navbar/>
-                <div className="center">
-                    <p className="name">BRB chat</p>
-                    <p className="left">Hello, Marie!</p>
-                    <p className="right">Ola, Brian :D</p>
-                    <p className="left">How was the lecture today?</p>
-                    <p className="right">It was good, we are studying Algebra now.</p>
-                    <p className="left-writing">Brian writing new message...</p>
+            <>
+                <div className="main">
+                    <Navbar/>
+                    <div className="hover-main">
+                        <div className="center">
+                            <p className="name">BRB chat</p>
+                            <p className="left">Hello, Marie!</p>
+                            <p className="right">Ola, Brian :D</p>
+                            <p className="left">How was the lecture today?</p>
+                            <p className="right">It was good, we are studying Algebra now.</p>
+                            <p className="left-writing">Brian writing new message...</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </>
         );
     }
 }
